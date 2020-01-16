48c37c5ba986712d47551249f12bc43331846637
Helenjw <50344290+Helenjw@users.noreply.github.com>
2020-01-15 20:46:26 -0500

    Update deliver.c

--- FileTransfer/pt3pt4/Deliver/deliver.c
+++ FileTransfer/pt3pt4/Deliver/deliver.c
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: jiang256
- *
- * Created on September 30, 2019, 7:59 PM
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
